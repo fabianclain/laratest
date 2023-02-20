33d8b36017cf25a31979583b02084d5437b2fff8
EMEA\clain <fabian.clain@hpe.com>
2023-02-20 10:32:33 +0200

    -

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -11,7 +12,7 @@ class ListingController extends Controller
     public function index(){
         // dd(request()->tag);
         return view('listings.index',[
-            'listings'=> Listing::latest()->filter(request(['tag']))->get()
+            'listings'=> Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
 
     }
@@ -20,5 +21,30 @@ class ListingController extends Controller
         return view('listings.show ',[
             'listing'=> $listing
         ]);
+    }
+    public function create(){
+        return view('listings.create');
+    }
+    //store listing date
+    public function store(Request $request){
+        // dd($request->file('logo'));
+
+        $formFields = $request -> validate([
+            'title'=> 'required',
+            'company'=> ['required', Rule::unique('listings', 'company')],
+            'location'=>'required',
+            'website'=> 'required',
+            'email'=>['required', 'email'], 
+            'tags'=>'required',
+            'description'=>'required'
+        ]);
+        if($request->hasFile('logo')){
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+        Listing::create($formFields);
+
+        return redirect('/')->with('message','Listing Created');
+        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,11 @@ use App\Models\Listing;
 //destroy
 
 Route::get('/', [ListingController::class, 'index']);
+
+Route::get ('/listings/create', [ListingController::class, 'create']);
+
+Route::post('/listings', [ListingController::class, 'store']);
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-
+//SHOW CREATE
 
